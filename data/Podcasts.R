21338f694d1f9ce686101c821bc5de7fe058ac25
gcho21 <gcho21@amherst.edu>
2020-10-09 00:47:52 -0400

    final script scraping

--- data/Podcasts.R
+++ data/Podcasts.R
@@ -1,17 +1,32 @@
 library(tidyverse)
 library(rvest)
 library(robotstxt)
+library(stringr)
+library(dplyr)
 
 paths_allowed('https://player.fm/podcasts/Women-Of-Color')
 
 podcasts_html <- read_html('https://player.fm/podcasts/Women-Of-Color')
 
+
 podcasts <- podcasts_html %>%
-  html_nodes('.title') %>%
+  html_nodes('.series:nth-child(n-1) .title') %>%
   html_text()
 
+podcasts <- podcasts %>%
+  stringr::str_replace_all("\n", "") %>%
+  stringr::str_replace_all('1', "") %>%
+  stringr::str_replace_all("4", "") %>%
+  stringr::str_replace_all("70", "")
+
+x <- c(podcasts[])
+podcasts <- unique(x)
+
+
+
+
 description <- podcasts_html %>%
-  html_nodes('series .content-main') %>%
+  html_nodes('.series:nth-child(n) .content-main') %>%
   html_text()
 
 podcasts_dat <- data.frame(podcast = podcasts, description = description
